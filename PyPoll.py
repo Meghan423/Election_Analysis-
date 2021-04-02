d55de57b2b792473e945dea522d75512d0a7cfc7
Meghan423 <80495032+Meghan423@users.noreply.github.com>
2021-04-02 10:05:56 -0400

    Updating now that the election audit code works
    this should give us the breakdown of vote percentage per candidate, and declare the winner and their vote count.

--- PyPoll.py
+++ PyPoll.py
@@ -50,8 +50,17 @@ with open(file_to_load) as election_data:
 # Iterate through the candidate list.
     for candidate_name in candidate_votes:
     # Retrieve vote count of a candidate.
-    votes = candidate_votes[candidate_name]
+        votes = candidate_votes[candidate_name]
     # Calculate the percentage of votes.
+        vote_percentage = float(votes) / float(total_votes) * 100
+
+
+# Determine the percentage of votes for each candidate by looping through the counts.
+# 1. Iterate through the candidate list.
+for candidate_name in candidate_votes:
+    # 2. Retrieve vote count of a candidate.
+    votes = candidate_votes[candidate_name]
+    # 3. Calculate the percentage of votes.
     vote_percentage = float(votes) / float(total_votes) * 100
 
     #  To do: print out each candidate's name, vote count, and percentage of
@@ -67,17 +76,9 @@ with open(file_to_load) as election_data:
          # And, set the winning_candidate equal to the candidate's name.
          winning_candidate = candidate_name
 
-# Determine the percentage of votes for each candidate by looping through the counts.
-# 1. Iterate through the candidate list.
-for candidate_name in candidate_votes:
-    # 2. Retrieve vote count of a candidate.
-    votes = candidate_votes[candidate_name]
-    # 3. Calculate the percentage of votes.
-    vote_percentage = float(votes) / float(total_votes) * 100
-
 # To do: print out each candidate's name, vote count, and percentage of
 # votes to the terminal.
-print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+    print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
 
 
 winning_candidate_summary = (
